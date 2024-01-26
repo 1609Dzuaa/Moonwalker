eef799ef9ed9cb8c73257fdcb9855ed814f0203d
huytl2107 <huytl2107@gmail.com>
2024-01-27 03:49:15 +0700

    fix bug

--- Assets/Scripts/Enums/GameEnums.cs
+++ Assets/Scripts/Enums/GameEnums.cs
@@ -5,12 +5,9 @@ using UnityEngine;
 public static class GameEnums 
 {
     public enum EPlayerState
-<<<<<<< HEAD
-    { idle, walk, jump, }
+    { idle, walk, jump, hatAttack }
 
     public enum EEnemiesState
     {idle, walk, attack, gothit}
-=======
-    { idle, walk, jump, hatAttack, }
->>>>>>> fa5640688a023746b3082433820ae7a4372da087
+
 }
