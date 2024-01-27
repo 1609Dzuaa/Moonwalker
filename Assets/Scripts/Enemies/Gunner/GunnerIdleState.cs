8e1e166a5ee08b1cea28ba59c31a64012350e179
huytl2107 <huytl2107@gmail.com>
2024-01-27 12:01:22 +0700

    pull

--- Assets/Scripts/Enemies/Gunner/GunnerIdleState.cs
+++ Assets/Scripts/Enemies/Gunner/GunnerIdleState.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GunnerIdleState : MonoBehaviour
+public class GunnerIdleState : EnemiesIdleState
 {
-    // Start is called before the first frame update
-    void Start()
+    public GunnerIdleState(EnemiesStateManager currentContext, EnemiesStateFactory currentState) : base(currentContext, currentState)
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void EnterState()
     {
-        
+        base.EnterState();
+    }
+
+    public override void UpdateState()
+    {
+        base.UpdateState();
+    }
+
+    public override void CheckSwitchState()
+    {
+        base.CheckSwitchState();
+    }
+
+    public override void ExitState()
+    {
+        base.ExitState();
     }
 }
