b29fc721253da12e7ad89f2d736aad27942b7fc8
1609Dzuaa <106080372+1609Dzuaa@users.noreply.github.com>
2024-01-27 19:44:49 +0700

    init SoundsManager

---
+++ Assets/Scripts/Managers/Sound.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public GameEnums.ESoundName _name;
+
+    public AudioClip _audioClip;
+}
--- Assets/Scripts/Managers/SoundsManager.cs
+++ Assets/Scripts/Managers/SoundsManager.cs
@@ -1,14 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static GameEnums;
 
 public class SoundsManager : Singleton<SoundsManager>
 {
+    [SerializeField]
+    Sound[] sfxSounds, musicSounds;
+    [SerializeField] AudioSource _sfxSource, _musicSource;
+
     public override void Awake()
     {
         base.Awake();
         DontDestroyOnLoad(gameObject);
     }
 
+    private void Start()
+    {
+        PlayMusic(ESoundName.Theme);
+    }
+
+    public void PlayMusic(ESoundName soundName)
+    {
+        Sound s = Array.Find(musicSounds, x => x._name == soundName);
+
+        if (s == null)
+            Debug.Log("Sound Not Found");
+        else
+        {
+            _musicSource.clip = s._audioClip;
+            _musicSource.Play();
+        }
+    }
+
+    public void PlaySFX(ESoundName name)
+    {
+        Sound s = Array.Find(musicSounds, x => x._name == name);
+
+        if (s == null)
+            Debug.Log("Sound Not Found");
+        else
+            _sfxSource.PlayOneShot(s._audioClip);
+    }
 
 }
